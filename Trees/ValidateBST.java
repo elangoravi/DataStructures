00bec402e6daf074e3e8a371f81a21283f2d45b5
Elango Ravi <65844981+elangoravi@users.noreply.github.com>
2020-05-24 12:32:10 +0530

    Update ValidateBST.java

--- Trees/ValidateBST.java
+++ Trees/ValidateBST.java
@@ -1,1 +1,49 @@
 
+package Trees;
+
+public class ValidateBST {
+
+	static class Node {
+		int data;
+		Node left, right;
+
+		Node(int data) {
+			this.data = data;
+			left = right = null;
+		}
+	}
+
+	static Node root;
+
+	public static Node insert(Node root, int val) {
+		if (root == null) {
+			return new Node(val);
+		} else {
+			if (val < root.data)
+				root.left = insert(root.left, val);
+			else
+				root.right = insert(root.right, val);
+		}
+		return root;
+	}
+
+	public static boolean valBST(Node n, Node lower, Node higher) {
+		if (n == null)
+			return true;
+		if (lower != null && n.data < lower.data)
+			return false;
+		if (higher != null && n.data > higher.data)
+			return false;
+		return valBST(n.left, lower, n) && valBST(n.right, n, higher);
+	}
+
+	public static void main(String[] args) {
+
+		root = new Node(5);
+		root.left = new Node(3);
+		root.right = new Node(7);
+		root.left.right = new Node(4);
+		System.out.println(valBST(root, null, null));
+	}
+
+}
